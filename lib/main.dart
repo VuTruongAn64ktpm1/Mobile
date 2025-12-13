b194fa3de102070eb70a2c829f1cb532d31ada06
Truongan <vutruongan15112003@gmail.com>
2025-12-13 07:50:35 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'An Tâm Nghe',
+      title: 'An Tâm Nghe 1',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primaryColor: AppColors.primaryBlue,
