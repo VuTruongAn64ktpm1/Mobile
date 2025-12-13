6d5337e24db50fc7ae63fc62460b789f6517a10d
Truongan <vutruongan15112003@gmail.com>
2025-12-13 07:56:09 +0700

    fix_icon

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'An Tâm Nghe 1',
+      title: 'An Tâm Nghe 24/7',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primaryColor: AppColors.primaryBlue,
