6aa98ecb5b183d31e4eea119dcc75ef815359fcc
Truongan <vutruongan15112003@gmail.com>
2025-12-18 07:42:05 +0700

    Update UI/feature/setting

--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -3,20 +3,50 @@ import '../../../core/app_colors.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: const BackButton(), title: const Text("Thiết lập")),
+      appBar: AppBar(
+        leading: const BackButton(),
+        title: const Text("Thiết lập"),
+      ),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          ListTile(leading: const CircleAvatar(backgroundColor: AppColors.avatarBlue), title: const Text("Nguyễn Văn A"), subtitle: const Text("Quản lý hồ sơ của bạn"), contentPadding: EdgeInsets.zero),
+          ListTile(
+            leading: const CircleAvatar(radius: 25, backgroundColor: AppColors.avatarBlue),
+            title: const Text("Nguyễn Văn A", style: TextStyle(fontWeight: FontWeight.bold)),
+            subtitle: const Text("Quản lý hồ sơ của bạn"),
+            contentPadding: EdgeInsets.zero,
+            onTap: () {
+            //dieu huong den profile screen
+            Navigator.pushNamed(context, '/profile');
+          },
+          ),
+          const SizedBox(height: 10),
           const Divider(),
-          _item(Icons.settings, "Chung"), _item(Icons.call, "Cuộc gọi"), _item(Icons.message_outlined, "Nhắn tin"), _item(Icons.block, "Chặn"), _item(Icons.info_outline, "Thông tin về ứng dụng"),
-          _item(Icons.help_outline, "Trợ giúp", onTap: () => Navigator.pushNamed(context, '/help')),
+          _item(context, Icons.settings_outlined, "Chung"),
+          _item(context, Icons.call_outlined, "Cuộc gọi", routeName: '/settings/call'), // Link tới trang Call Settings
+          _item(context, Icons.chat_bubble_outline, "Nhắn tin"),
+          _item(context, Icons.block_outlined, "Chặn"),
+          _item(context, Icons.info_outline, "Thông tin về ứng dụng", routeName: '/settings/info'), // Link tới trang App Info
+          _item(context, Icons.help_outline, "Trợ giúp", routeName: '/help'),
         ],
       ),
     );
   }
-  Widget _item(IconData i, String t, {VoidCallback? onTap}) => ListTile(leading: Icon(i, color: Colors.black), title: Text(t), onTap: onTap, contentPadding: EdgeInsets.zero);
+
+  Widget _item(BuildContext context, IconData icon, String text, {String? routeName}) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black87),
+      title: Text(text),
+      contentPadding: EdgeInsets.zero,
+      onTap: () {
+        if (routeName != null) {
+          Navigator.pushNamed(context, routeName);
+        }
+      },
+    );
+  }
 }
